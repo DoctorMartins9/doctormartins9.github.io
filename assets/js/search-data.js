cc44f54d85a881a0149c061c92b5297ceb91bae7
DoctorMartins9 <45597184+DoctorMartins9@users.noreply.github.com>
2026-04-08 08:17:48 +0000

    Deploying to gh-pages from @ DoctorMartins9/doctormartins9.github.io@f9d70b2ab95c4ae21a0200795f071b4d7c7bc81e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -385,7 +385,7 @@ ninja.data = [{
               window.location.href = "/books/the_godfather.html";
             },},{id: "projects-human-motion-estimation-and-filtering",
           title: 'Human Motion Estimation and Filtering',
-          description: "A research line on robust human motion intelligence, from noisy pose refinement to sparse sensing and multiview tracking.",
+          description: "A research line on robust human motion modelling, from noisy pose refinement to sparse sensing and multiview tracking.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
             },},{id: "projects-markerless-gait-analysis-and-digital-motor-assessment",
